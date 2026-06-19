4f1f57f325286bded7ffec83c2ab567a489fb894
Namrita <namrita057@gmail.com>
2026-06-20 01:40:22 +0530

    Added project files

---
+++ src/main/java/com/mycompany/digitallockersystem/DigitalLockerSystem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.digitallockersystem;
+
+/**
+ *
+ * @author hp
+ */
+public class DigitalLockerSystem {
+
+    public static void main(String[] args) {
+
+        LoginForm login = new LoginForm();
+        login.setVisible(true);
+
+    }
+}
+    
